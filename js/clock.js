6f43c1e5a19d946deb5b390f50abca6dfb8c9c84
Nəzrin <isbndyrva@gmail.com>
2021-12-27 20:47:38 +0400

    Update analog clock

--- js/clock.js
+++ js/clock.js
@@ -16,3 +16,14 @@ function ticktock() {
 
 setInterval(ticktock, 1000);
 ticktock();
+
+// cursor
+let cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+});
